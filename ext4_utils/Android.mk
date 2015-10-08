650426f7e0907aeb2bc0b9de024671dc5efabf85
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-10-08 02:02:47 +0200

    ext4_utils: Yet another MMC discard pain in the ass:
    Ported from 5.1:
    https://github.com/XXMrHyde/android_system_extras/commit/f74edca02285bcc00ac55c155a083d60c8dbf1df
    https://github.com/XXMrHyde/android_system_extras/commit/87d37b0ea38c35f075fefb70992ec715cd7e75c8

--- ext4_utils/Android.mk
+++ ext4_utils/Android.mk
@@ -70,6 +70,13 @@ LOCAL_SHARED_LIBRARIES := \
     libsparse \
     libz
 LOCAL_CFLAGS := -DREAL_UUID
+
+# Some MMCs have VERY slow secure erase - use
+#standard erase instead
+ifeq ($(BOARD_SUPPRESS_SECURE_ERASE),true)
+    LOCAL_CFLAGS += -DSUPPRESS_SECURE_ERASE
+endif
+
 include $(BUILD_SHARED_LIBRARY)
 
 
@@ -79,6 +86,13 @@ LOCAL_SRC_FILES := $(libext4_utils_src_files) \
 LOCAL_MODULE := libext4_utils_static
 LOCAL_STATIC_LIBRARIES := \
     libsparse_static
+
+# Some MMCs have VERY slow secure erase - use
+#standard erase instead
+ifeq ($(BOARD_SUPPRESS_SECURE_ERASE),true)
+    LOCAL_CFLAGS += -DSUPPRESS_SECURE_ERASE
+endif
+
 include $(BUILD_STATIC_LIBRARY)
 
 
